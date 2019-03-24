b40e9cb99407948801ebe32942697221db0caca2
sakuopon730 <mcz622730@gmail.com>
2019-03-24 18:38:23 +0900

    no message

--- script.js
+++ script.js
@@ -1,20 +1,9 @@
-// //moreのクリック処理
-// $(function(){
-//     $('.click-more').on('click', function(e){
-//         e.preventDefault();
-//         $('.modal-area').fadeIn();//※修正
-//     })
-//     $('.close, .modal-bg').on('click', function(e){
-//         $('.modal-area').fadeOut();//※修正
-//     });
-// });
-
 //1ページ目のスクロールボタン
 $(function(){
     $('#arrow-btn-ft').click(function(e){
         e.preventDefault();
         $('html').animate({
-            scrollTop:667//※数値修正
+            scrollTop:667//※数値修正(数値じゃなくする)
         }, 800);
     });
 });
@@ -25,7 +14,7 @@ $(function(){
     $('#arrow-btn-sd').click(function(e){
         e.preventDefault();
         $('html').animate({
-            scrollTop:1334//※数値修正
+            scrollTop:1334//※数値修正(数値じゃなくする)
         }, 800);
     });
 });
@@ -42,23 +31,35 @@ $(function(){
 
 
 
-/* スクロールアニメーション */
-$('.contents-index').css('visibility', 'visible');
-$(window).scroll(function(){
-    var windowHeight = $(window).height();
-    var topWindow = $(window).scrollTop();
+// /* スクロールアニメーション */
+// $('.contents-index').css('visibility', 'visible');
+// $(window).scroll(function(){
+//     var windowHeight = $(window).height();
+//     var topWindow = $(window).scrollTop();
 
-    $('.down-button-bk').each(function(){
-        var targetPosition = $(this).offset().top;
-        if(topWindow > targetPosition - windowHeight + 30){
-            $(this).addClass("bkButtonFadeInUp");
-        }
-    });
+//     $('.down-button-bk').each(function(){
+//         var targetPosition = $(this).offset().top;
+//         if(topWindow > targetPosition - windowHeight + 30){
+//             $(this).addClass("bkButtonFadeInUp");
+//         }
+//     });
 
-    $('.contents-index, .title, .intro-txt, .ct-ttl, .pf-img, .profile, .comment, .works-item, .contact').each(function(){
-        var targetPosition = $(this).offset().top;
-        if(topWindow > targetPosition - windowHeight + 80){
-            $(this).addClass("scrollFadeInUp");
-        }
+//     $('.contents-index, .title, .intro-txt, .ct-ttl, .pf-img, .profile, .comment, .works-item, .contact').each(function(){
+//         var targetPosition = $(this).offset().top;
+//         if(topWindow > targetPosition - windowHeight + 80){
+//             $(this).addClass("scrollFadeInUp");
+//         }
+//     });
+// });
+
+
+//moreのクリック処理
+$(function(){
+    $('.click-more').on('click', function(e){
+        e.preventDefault();
+        $('.modal-area').fadeIn();//※修正（cssでfadeIn）
+    })
+    $('.close, .modal-bg').on('click', function(e){
+        $('.modal-area').fadeOut();//※修正（cssでfadeOut）
     });
 });
