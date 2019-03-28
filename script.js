69a6904e906a3b16fbf9c8a96fffe256c276f517
sakuopon730 <mcz622730@gmail.com>
2019-03-28 16:26:05 +0900

    no message

--- script.js
+++ script.js
@@ -1,52 +1,79 @@
-// //1ページ目のスクロールボタン
-// $(function(){
-//     $('#arrow-btn-ft').click(function(e){
-//         e.preventDefault();
-//         $('html').animate({
-//             scrollTop:767//※数値修正(数値じゃなくする)
-//         }, 800);
-//     });
-// });
-
-
-// //2ページ目のスクロールボタン
-// $(function(){
-//     $('#arrow-btn-sd').click(function(e){
-//         e.preventDefault();
-//         $('html').animate({
-//             scrollTop:1534//※数値修正(数値じゃなくする)
-//         }, 800);
-//     });
-// });
-
-
-
-//スムーススクロールの処理
-var current;
-
-var option = {
-    section: '.box',
-    setHeights:false,
-    easing:'swing',
-    scrollSpeed:1000,
-    before:function(i,box){
-      current = i;
-    },
-};
-$(window).on('resize',function(e){
-    e.preventDefault();
-    if(current){
-        var currentScrl = $('.box').eq(current).offset().top;
-        $(window).scrollTop(currentScrl);
-    }
-});
+/********** スムーススクロールの処理 **********/
 $(function(){
-    $.scrollify(option);
-});
+    var current;
+    var w = $(window).width();
+
+    if(w >= 601 && 960 >= w){//横幅601~960pxの時のスムーススクロール
+        $.scrollify({
+            section: '.large',
+            setHeights:false,
+            easing:'swing',
+            scrollSpeed:1000,
+            before:function(i,large){
+            current = i;
+            },
+        });
+        $(window).on('resize',function(){
+            if(current){
+                var currentScrl = $('.large').eq(current).offset().top;
+                $(window).scrollTop(currentScrl);
+            }
+        });    
+    }else if(w >= 481 && 600 >= w){//横幅481~600pxの時のスムーススクロール
+        $.scrollify({
+            section: '.midi',
+            setHeights:false,
+            easing:'swing',
+            scrollSpeed:1000,
+            before:function(i,midi){
+            current = i;
+            },
+        });
+        $(window).on('resize',function(){
+            if(current){
+                var currentScrl = $('.midi').eq(current).offset().top;
+                $(window).scrollTop(currentScrl);
+            }
+        });    
+    }else if(w <= 480){//横幅480px以下時のスムーススクロール
+        $.scrollify({
+            section: '.small',
+            setHeights:false,
+            easing:'swing',
+            scrollSpeed:1000,
+            before:function(i,small){
+            current = i;
+            },
+        });
+        $(window).on('resize',function(){
+            if(current){
+                var currentScrl = $('.small').eq(current).offset().top;
+                $(window).scrollTop(currentScrl);
+            }
+        });    
+    }else{//通常サイズ時のスムーススクロール
+        $.scrollify({
+            section: '.nomal',
+            setHeights:false,
+            easing:'swing',
+            scrollSpeed:1000,
+            before:function(i,nomal){
+            current = i;
+            },
+        });
+        $(window).on('resize',function(){
+            if(current){
+                var currentScrl = $('.nomal').eq(current).offset().top;
+                $(window).scrollTop(currentScrl);
+            }
+        });    
+    }
+});    
 
 
 
-//ハンバーガーメニュー
+
+/********** ハンバーガーメニュー **********/
 $(function(){
     $('#menu-btn, #menu-bg').on('click',function(){
       $(this).toggleClass('active');
@@ -55,11 +82,13 @@ $(function(){
 });
 
 
-//moreのクリック処理
+
+
+/********** moreのクリック処理 **********/
 $(function(){
 
     //開くときの処理
-    $('.click-more').on('click', function(e){ //.modal-bgを消した
+    $('.click-more').on('click', function(e){
         e.preventDefault();
         $('.modal-area').toggleClass('active');
     });
@@ -74,26 +103,37 @@ $(function(){
 
 
 
-// /* スクロールアニメーション */
+
+/********** スクロールアニメーションの記述 **********/
 $('.contents-index').css('visibility', 'visible');
 $(window).scroll(function(){
     var windowHeight = $(window).height();
     var topWindow = $(window).scrollTop();
+    var windowWidth = $(window).width();
 
-
-    // $('.down-button-bk').each(function(){
-    //     var targetPosition = $(this).offset().top;
-    //     if(topWindow > targetPosition - windowHeight + 30){
-    //         $(this).addClass('bkButtonFadeInUp');
-    //     }
-    // });
-
-    $('.contents-index, .title, .intro-txt, .ct-ttl, .pf-img, .profile, .comment, .works-item, .contact, .down-button-bk, .btnOfSmall').each(function(){
-        var targetPosition = $(this).offset().top;
-        if(topWindow > targetPosition - windowHeight + 40){
-            $(this).addClass('scrollFadeInUp');
-        }
-    });
+    if(windowWidth >= 960){
+        //通常サイズ時のアニメーション
+        $('.contents-index, .title, .intro-txt, .ct-ttl, .pf-img, .profile, .comment, .works-item, .contact, .down-button-bk, .btnOfSmall').each(function(){
+            var targetPosition = $(this).offset().top;
+            if(topWindow > targetPosition - windowHeight + 40){
+                $(this).addClass('scrollFadeInUp');
+            }
+        });
+    }else{
+        //レスポンシブ時のアニメーション
+        $('.contents-index, .title, .intro-txt, .ct-ttl, .pf-img, .profile, .comment, .works-item, .contact, .down-button-bk, .btnOfSmall').each(function(){
+            var targetPosition = $(this).offset().top;
+            if(topWindow > targetPosition - windowHeight + 120){
+                $(this).addClass('scrollFadeInUp');
+            }
+        });
+        $('.title, .intro-txt, .btnOfSmall').each(function(){
+            var targetPosition = $(this).offset().top;
+            if(topWindow > targetPosition - windowHeight + 40){
+                $(this).addClass('scrollFadeInUp');
+            }
+        });
+    }
 });
 
 
