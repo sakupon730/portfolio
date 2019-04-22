6429c174a446d732861b1fc57c986989298caa68
sakuopon730 <mcz622730@gmail.com>
2019-04-22 17:11:52 +0900

    お問い合わせフォーム実装

---
+++ confirmation.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8" />
+  </head>
+  <body>
+    <?php
+      mb_language("Japanese");
+      mb_internal_encoding("UTF-8");
+
+      $name = $_POST['user-name'];
+      $mail = $_POST['user-mail'];
+      $msg = $_POST['inquiry'];
+
+      if(mb_send_mail($name, $mail, $msg)){
+        echo "メールを送信しました";
+      } else {
+        echo "メールの送信に失敗しました";
+      };
+    ?>
+  </body>
+</html>
